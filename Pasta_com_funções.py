2948b490e5f45a77ed5cdc5aee254cf62618912b
andreimp2021 <andreimp@al.insper.edu.br>
2021-05-09 01:52:57 -0300

    mais uma função feita depois de longas horas

---
+++ Pasta_com_funções.py
@@ -0,0 +1,62 @@
+def cria_baralho():
+
+    lista = ["A♠" ,"K♠" ,"Q♠" ,"J♠" ,"10♠" ,"9♠" ,"8♠" ,"7♠" ,"6♠" ,"5♠" ,"4♠" ,"3♠" ,"2♠" ,"A♥" ,"K♥" ,"Q♥" ,"J♥" ,"10♥" ,"9♥" ,"8♥" ,"7♥" ,"6♥" ,"5♥" ,"4♥", "3♥" ,"2♥" ,"A♦" ,"K♦" ,"Q♦" ,"J♦" ,"10♦" ,"9♦" ,"8♦" ,"7♦" ,"6♦" ,"5♦" ,"4♦" ,"3♦" ,"2♦" ,"A♣" ,"K♣" ,"Q♣" ,"J♣" ,"10♣" ,"9♣" ,"8♣" ,"7♣" ,"6♣" ,"5♣" ,"4♣" ,"3♣" ,"2♣"]
+
+    return lista 
+
+def extrai_naipe(carta):
+    if len(carta) <= 2:
+        e = carta[1]
+        return e
+    elif len(carta) == 3:
+        e = carta[2]
+        return e
+
+def extrai_valor(carta):
+    if len(carta) == 2:
+        e = carta[0]
+        return e
+    elif len(carta) == 3:
+        e = carta[0] + carta[1]
+        return e 
+
+# Aqui são apenas funções anteriores
+
+def extrai_naipe(carta):
+    if len(carta) <= 2:
+        e = carta[1]
+        return e
+    elif len(carta) == 3:
+        e = carta[2]
+        return e
+
+def extrai_valor(carta):
+    if len(carta) == 2:
+        e = carta[0]
+        return e
+    elif len(carta) == 3:
+        e = carta[0] + carta[1]
+        return e 
+
+# começa aqui
+        
+
+def lista_movimentos_possiveis(lista, indice):
+    lista2 = []
+    if indice == 0:
+        return []
+
+    if extrai_naipe(lista[indice]) == extrai_naipe(lista[indice-1]) or  extrai_valor(lista[indice]) == extrai_valor(lista[indice-1]):
+             lista2.append(1)
+
+    if indice >= 3 and (extrai_naipe(lista[indice]) == extrai_naipe(lista[indice-3]) or  extrai_valor(lista[indice]) == extrai_valor(lista[indice-3])):
+             lista2.append(3)
+             
+    return lista2
+  
+        
+
+  
+
+
+    
