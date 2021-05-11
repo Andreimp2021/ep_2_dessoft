c03b15376f036cca0b41bb966ea5a7357ed88f91
andreimp2021 <andreimp@al.insper.edu.br>
2021-05-11 01:40:24 -0300

    mais um progresso depois de longas horas

--- jogo.py
+++ jogo.py
@@ -1,3 +1,4 @@
+from math import sqrt
 import Pasta_com_funções
 from random import sample
   
@@ -25,25 +26,64 @@ print("")
 
 lista = ["A♠" ,"K♠" ,"Q♠" ,"J♠" ,"10♠" ,"9♠" ,"8♠" ,"7♠" ,"6♠" ,"5♠" ,"4♠" ,"3♠" ,"2♠" ,"A♥" ,"K♥" ,"Q♥" ,"J♥" ,"10♥" ,"9♥" ,"8♥" ,"7♥" ,"6♥" ,"5♥" ,"4♥", "3♥" ,"2♥" ,"A♦" ,"K♦" ,"Q♦" ,"J♦" ,"10♦" ,"9♦" ,"8♦" ,"7♦" ,"6♦" ,"5♦" ,"4♦" ,"3♦" ,"2♦" ,"A♣" ,"K♣" ,"Q♣" ,"J♣" ,"10♣" ,"9♣" ,"8♣" ,"7♣" ,"6♣" ,"5♣" ,"4♣" ,"3♣" ,"2♣"]
 
-x = sample(lista,52)
+baralho = sample(lista,52)
 i = 1
 while i <= 52:
     print("{0}. {1}".format(i,x[i-1]))
     i += 1
 
 print("")
-a = input("Escolha uma carta (digite um número entre 1 e 52): ")
-print("")
+indice = int(input("Escolha uma carta (digite um número entre 1 e 52): "))
+
+lista2 = []
 
 while Pasta_com_funções.possui_movimentos_possiveis(lista) == True:
-    if Pasta_com_funções.possui_movimentos_possiveis(lista) == True:
-        a = input("Escolha uma carta (digite um número entre 1 e 52): ")
-    else:
-        Pasta_com_funções. possui_movimentos_possiveis(lista) = False:
+    if Pasta_com_funções.lista_movimentos_possiveis(baralho, indice) == [1]:
+        destino = baralho[indice-1]
+        baralho = Pasta_com_funções.empilha(baralho,indice, destino)
+    
+    elif  Pasta_com_funções.lista_movimentos_possiveis(baralho, indice) == [3]:
+        destino = baralho[indice-3]
+        baralho = Pasta_com_funções.empilha(baralho,indice, destino)
 
-while Pasta_com_funções.possui_movimentos_possiveis(lista) == False:
-    b = input("A carta {a} não pode ser movida. Por favor, digite um número entre 1 e 52".format(a))
-if Pasta_com_funções.possui_movimentos_possiveis(lista) == True
- Pasta_com_funções. possui_movimentos_possiveis(lista) = False:
+    elif Pasta_com_funções.lista_movimentos_possiveis(baralho, indice) ==  [1 ,3]:
+        pergunta = int(input("Sobre qual carta você quer empilhar o {0}?\n 1. {1}\n 2.{2} ".format(baralho[indice], baralho[indice-1], baralho[indice-3])))
+        if pergunta == 1:
+            destino = baralho[indice-1]
+
+        elif pergunta == 2:
+            destino = baralho[indice-3]
+
+        else:
+            while pergunta != 1 or pergunta != 2:
+                pergunta = int(input("Opção inválida. Sobre qual carta você quer empilhar o {0}?\n 1. {1}\n 2.{2} ".format(baralho[indice], baralho[indice-1], baralho[indice-3])))
+            if pergunta == 1:
+                destino = baralho[indice-1]
+
+            elif pergunta == 2:
+                destino = baralho[indice-3]
+
+        baralho = Pasta_com_funções.empilha(baralho,indice, destino)
+    else:
+        while indice > len(baralho) or indice < 1:
+            print("Posição inválida. Por favor, digite um número entre 1 e {0}: ".format(len(baralho)))
+            
+
+
+
+
+
+
+
+            print("")
+            # empilhar e retornar lista
+    
+        else:
+            b = input ("A carta {0} não pode ser movida. Por favor, digite um número entre 1 e 52: ".format(indice)) 
+            # empilhar e retornar baralho
+    pri
+print("você perdeu :(")
+
+  
 
 
